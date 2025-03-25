263e99d31c41cc08b61ca466d0165b74d131a1ba
“ST <shaotian.li.april@gmail.com>
2025-03-25 11:48:42 +0800

    test deploy-5

--- miniboard-ng/src/environments/environment.prod.ts
+++ miniboard-ng/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://node-minimsgboard-1.onrender.com/'
-    // apiUrl: 'https://miniboard-backend.onrender.com'
+    apiUrl: 'https://node-minimsgboard-1.onrender.com'
   };
